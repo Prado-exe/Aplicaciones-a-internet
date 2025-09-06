786f752a83d867137091855dbcc43adf59959e03
MatiasWormald <matias.wormald.xd@gmail.com>
2025-09-05 20:41:19 -0400

    soy entero pro avanzo todox2

--- Fablab/src/App.jsx
+++ Fablab/src/App.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from 'react';
 import Navbar from './componentes/Navbar';
-import './styles/App.css'; // importa los estilos
 import UniqueDivider from './componentes/UniqueDivider';
+import Carousel from './componentes/Carousel';
+import './styles/App.css';
 
-// Componente separador SVG editable
+
 
 function App() {
   const [opacity, setOpacity] = useState(1);
@@ -29,38 +30,33 @@ function App() {
           className="hero-img"
         />
         <div className="gradient-overlay" />
-        <h1 className="hero-text">Fablab Hola Mundo aaaaaria</h1>
+        <h1 className="hero-text">FABLAB Y EL PVP CONTRA PERU</h1>
       </section>
 
       <UniqueDivider />
 
       <section className="carousel-section">
-        {/* Aquí va tu carrusel de novedades */}
-        <p className="body-text">Carrusel de novedades</p>
+        <Carousel />
       </section>
 
       <UniqueDivider />
 
       <section className="events-section">
-        {/* Aquí eventos/talleres */}
         <p className="body-text">Próximos eventos / talleres</p>
       </section>
 
       <UniqueDivider />
 
       <section className="news-section">
-        {/* Noticias destacadas */}
         <p className="body-text">Noticias destacadas</p>
       </section>
 
       <UniqueDivider />
 
       <footer className="footer-section">
-        {/* Pie de página: contacto, redes, colaboradores */}
         <p className="body-text">Pie de página con info de contacto y redes</p>
       </footer>
 
-      {/* Bloque test scroll abajo */}
       <div className="scroll-test">
         <div className="scroll-end-text">
           <h2>Aquí termina la página (test scroll)</h2>
