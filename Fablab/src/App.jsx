c595aaf9ff4939a523f01d774f3ff2c30c0e7183
Bastian <bastianpra2@gmail.com>
2025-09-12 20:18:32 -0300

    [footbar] : Desarrollo de footbar lo termino mañana

--- Fablab/src/App.jsx
+++ Fablab/src/App.jsx
@@ -3,6 +3,9 @@ import Navbar from './componentes/Navbar';
 import UniqueDivider from './componentes/UniqueDivider';
 import Carousel from './componentes/Carousel';
 import './styles/App.css';
+import Footer from './componentes/footbar';
+import fablabImg from './assets/fablab_test.png';
+
 
 function App() {
   const [opacity, setOpacity] = useState(1);
@@ -10,19 +13,32 @@ function App() {
   useEffect(() => {
     function handleScroll() {
       const scrollTop = window.scrollY;
-      const newOpacity = Math.max(1 - (scrollTop / 400) * 0.6, 0);
+      const newOpacity = Math.max(1 - (scrollTop / 400)*0.6, 0);
       setOpacity(newOpacity);
     }
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
+  
+  
 
   return (
     <>
       <Navbar />
 
-      {/* Sección principal con el carrusel */}
       <section className="hero-section" style={{ opacity }}>
+        <img
+          src={fablabImg}
+          alt="Imagen inicial"
+          className="hero-img"
+        />
+        <div className="gradient-overlay" />
+        <h1 className="hero-text">"Bienvenidos al sitio web del FABLAB FIULS"</h1>
+      </section>
+
+      <UniqueDivider />
+
+      <section className="carousel-section">
         <Carousel />
       </section>
 
@@ -39,16 +55,9 @@ function App() {
       </section>
 
       <UniqueDivider />
+      
+      <Footer />
 
-      <footer className="footer-section">
-        <p className="body-text">Pie de página con info de contacto y redes</p>
-      </footer>
-
-      <div className="scroll-test">
-        <div className="scroll-end-text">
-          <h2>Aquí termina la página (test scroll)</h2>
-        </div>
-      </div>
     </>
   );
 }
